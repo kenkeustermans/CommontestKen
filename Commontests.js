26cd9bd9499ed688589fc57c6e9c7fbe3e9c77ae
kenkeustermans <39733186+kenkeustermans@users.noreply.github.com>
2019-05-27 10:11:54 +0200

    Update Commontests.js

--- Commontests.js
+++ Commontests.js
@@ -6,27 +6,27 @@
  * @author  Ken Keustermans
  */
 function commonTest(responseCode, contentType, time) {
-    if (responseCode >= 200 && responseCode <= 399) {
-        it('should be a successful response', () => {
-            response.should.have.status(responseCode);
-        });
-    } else if (responseCode >= 400 && responseCode <= 599) {
-        it('should be a unsuccessful response', () => {
-            response.should.have.status(responseCode);
-        });
-    }
-    
-    if (time){
-        it('should respond in a timely manner', () => {
-            response.time.should.be.below(time);
-        });
-    }
-    
-    if(contentType){
-        it('should return ' + contentType, () => {
-            response.should.have.header('Content-Type', contentType);
-        });
-    }
+  if (responseCode >= 200 && responseCode <= 399) {
+      it('should be a successful response', () => {
+          response.should.have.status(responseCode);
+      });
+  } else if (responseCode >= 400 && responseCode <= 599) {
+      it('should be a unsuccessful response', () => {
+          response.should.have.status(responseCode);
+      });
+  }
+
+  if (time){
+      it('should respond in a timely manner', () => {
+          response.time.should.be.below(time);
+      });
+  }
+
+  if (contentType){
+      it('should return ' + contentType, () => {
+          response.should.have.header('Content-Type', contentType);
+      });
+  }
 }
 function commonTestWithoutTime(responseCode, contentType,) {
     if (responseCode >= 200 && responseCode <= 399) {
@@ -39,9 +39,39 @@ function commonTestWithoutTime(responseCode, contentType,) {
         });
     }
     
-    if(contentType){
+    if (contentType){
         it('should return ' + contentType, () => {
             response.should.have.header('Content-Type', contentType);
         });
     }
+}
+function commonTestWithSchema(responseCode, contentType, time, jsonSchema) {
+  if (responseCode >= 200 && responseCode <= 399) {
+      it('should be a successful response', () => {
+          response.should.have.status(responseCode);
+      });
+  } else if (responseCode >= 400 && responseCode <= 599) {
+      it('should be a unsuccessful response', () => {
+          response.should.have.status(responseCode);
+      });
+  }
+
+  if (time){
+      it('should respond in a timely manner', () => {
+          response.time.should.be.below(time);
+      });
+  }
+
+  if (contentType){
+      it('should return ' + contentType, () => {
+          response.should.have.header('Content-Type', contentType);
+      });
+  }
+  
+  if (jsonSchema){
+    it('should match against a JSON Schema', () => {
+    // For more information about JSON Schema, see https://spacetelescope.github.io/understanding-json-schema/basics.html
+        response.body.should.have.schema(jsonSchema);
+    });
+  }
 }
